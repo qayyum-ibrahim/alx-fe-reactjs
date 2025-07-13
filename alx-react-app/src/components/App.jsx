27418dae356336c6180551af9034c007ec5d3e8a
Qayyum Ibrahim <temiyores1234@gmail.com>
2025-07-13 20:43:01 +0100

    header, main content and footer components created

---
+++ alx-react-app/src/components/App.jsx
@@ -0,0 +1,18 @@
+import WelcomeMessage from "./WelcomeMessage";
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <WelcomeMessage />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
