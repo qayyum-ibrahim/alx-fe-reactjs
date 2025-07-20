5d5352c15d3c17ef1937f311d7f8de596040837a
Qayyum Ibrahim <temiyores1234@gmail.com>
2025-07-20 14:04:55 +0100

    fixed style error

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,25 +1,34 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const navStyle = {
-    display: 'flex',
-    gap: '15px',
-    padding: '10px 20px',
-    backgroundColor: '#222',
-    color: 'white',
+    display: "flex",
+    gap: "15px",
+    padding: "10px 20px",
+    backgroundColor: "#222",
+    color: "white",
+    justifyContent: "center",
   };
 
   const linkStyle = {
-    color: 'white',
-    textDecoration: 'none',
+    color: "white",
+    textDecoration: "none",
   };
 
   return (
     <nav style={navStyle}>
-      <Link style={linkStyle} to="/">Home</Link>
-      <Link style={linkStyle} to="/about">About</Link>
-      <Link style={linkStyle} to="/services">Services</Link>
-      <Link style={linkStyle} to="/contact">Contact</Link>
+      <Link style={linkStyle} to="/">
+        Home
+      </Link>
+      <Link style={linkStyle} to="/about">
+        About
+      </Link>
+      <Link style={linkStyle} to="/services">
+        Services
+      </Link>
+      <Link style={linkStyle} to="/contact">
+        Contact
+      </Link>
     </nav>
   );
 }
