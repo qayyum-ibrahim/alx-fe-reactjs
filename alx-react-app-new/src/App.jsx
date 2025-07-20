08a9a64841360875447e9b897f3b16927dafca24
Qayyum Ibrahim <temiyores1234@gmail.com>
2025-07-20 13:24:24 +0100

    updated components with inline styles

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,20 @@
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import "./App.css";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <WelcomeMessage />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+const MainContent = () => {
+  return (
+    <main
+      style={{ backgroundColor: "navy", color: "white", textAlign: "center" }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+};
+
+export default MainContent;
