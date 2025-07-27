60470cc5c8d355bf1b4f3914cb375920917156a9
Qayyum Ibrahim <temiyores1234@gmail.com>
2025-07-27 14:08:06 +0100

    recipe updated

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,8 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import { useRecipeStore } from "./recipeStore";
 
 const RecipeList = () => {
   const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
-
+  const navigate = useNavigate();
   if (filteredRecipes.length === 0) return <p>No matching recipes.</p>;
 
   return (
@@ -10,9 +11,9 @@ const RecipeList = () => {
       <h2>Filtered Recipes</h2>
       {filteredRecipes.map((recipe) => (
         <div key={recipe.id} className="mb-4 p-2 border">
-          <Link to={`/recipe/${recipe.id}`}>
+          <div onClick={() => navigate(`/recipe/${recipe.id}`)}>
             <h3 className="text-blue-600 underline">{recipe.title}</h3>
-          </Link>
+          </div>
         </div>
       ))}
     </div>
