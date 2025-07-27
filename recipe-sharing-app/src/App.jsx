c2f19f905f5f9b8ed2d0bab6c9c200f571c64ffe
Qayyum Ibrahim <temiyores1234@gmail.com>
2025-07-27 13:23:15 +0100

    recipe-sharing-app completed

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,13 @@
+import AddRecipeForm from "../components/AddRecipeForm";
+import RecipeList from "../components/RecipeList";
+
+const App = () => {
+  return (
+    <div>
+      <RecipeList />
+      <AddRecipeForm />
+    </div>
+  );
+};
+
+export default App;
