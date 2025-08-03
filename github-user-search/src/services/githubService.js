a33847990c8ba2acf7348875c5b8a2b57736efa5
Qayyum Ibrahim <temiyores1234@gmail.com>
2025-08-03 14:41:15 +0100

    github-user-search update

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -4,7 +4,9 @@ const BASE_URL = import.meta.env.VITE_APP_GITHUB_API_URL;
 
 export const fetchUserData = async (username) => {
   try {
-    const response = await axios.get(`${BASE_URL}/${username}`);
+    const response = await axios.get(
+      `https://api.github.com/users/${username}`
+    );
     return response.data;
   } catch (error) {
     throw new Error("User not found");
