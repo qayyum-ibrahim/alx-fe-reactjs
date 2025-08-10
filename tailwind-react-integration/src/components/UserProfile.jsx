a1cbf8275cde72576abb619a0041f15a28c7d332
Qayyum Ibrahim <temiyores1234@gmail.com>
2025-08-10 13:06:16 +0100

    updated user profile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,6 @@
-// src/components/UserProfile.jsx
-import React from "react";
-
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
