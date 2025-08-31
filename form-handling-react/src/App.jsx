961b5a461ee88ddfffe68b4bcccb7dd93d24e806
Qayyum Ibrahim <temiyores1234@gmail.com>
2025-08-31 21:07:26 +0100

    updated file name

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,5 +1,5 @@
-import RegistrationForm from "./components/RegistrationForm";
 import FormikForm from "./components/FormikForm";
+import RegistrationForm from "./components/RegistrationForm";
 
 function App() {
   return (
